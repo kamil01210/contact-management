d63add2c023e337a6742d3c229ae1725ff71767a
Kamil <kuaku@pc3.home>
2017-03-30 17:12:30 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kuaku on 30.03.2017.
+ */
+
+$(document).ready(function (){
+
+    $('.push-form').click(function() {
+        $('.forms').toggleClass('form-slide');
+    });
+});
